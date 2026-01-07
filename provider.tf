59ad30f8751cd0d71cde8c4f934aef1aa16eb67b
Filipe de Morais Pereira <fpereira@btcreditos.com.br>
2026-01-07 12:47:21 -0300

    feat: ajustes

--- provider.tf
+++ provider.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
